0538dbb5e2895ed6d868328fffdd711d68abe7ca
sombhu2025 <somnathbhute@gmail.com>
2025-06-03 15:50:26 +0530

    mmt to Make My Trip

--- src/main/java/com/example/makemytrip/hotels.java
+++ src/main/java/com/example/makemytrip/hotels.java
@@ -6,5 +6,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class hotels {
     @GetMapping("/hotels")
-    public String getData() {return  "please book your hotels from mmt kindly book ticket for primium hotels in Delhi" ; }
+    public String getData() {return  "please book your hotels from Make my Trip kindly book ticket for primium hotels in Delhi" ; }
 }
